e44c250cd0f858e6301fe8f477b2ea57039fb3e0
Ivan Krivonos <devbackend@yandex.ru>
2024-04-17 18:36:37 +0400

    Move debug

--- main.go
+++ main.go
@@ -18,24 +18,21 @@ const (
 	colorReset = "\033[0m"
 )
 
+var start time.Time
+
 func main() {
 	os.Exit(run())
 }
 
 func run() int {
+	start = time.Now()
+
 	if len(os.Args) < 2 {
 		printWarning("not found command in rows")
 		return exitError
 	}
 
-	isDebug := envParam("ZSH_PLUGINS_OH_MY_ALIASES_DEBUG", "0") == "1"
 	expandAlias := envParam("ZSH_PLUGINS_OH_MY_ALIASES_EXPAND_ALIAS", "0") == "1"
-
-	if isDebug {
-		defer func(start time.Time) {
-			printDebug(fmt.Sprintf("[debug] time: %d μs", time.Since(start).Microseconds()))
-		}(time.Now())
-	}
 
 	rows := make([]string, 0)
 
@@ -67,15 +64,16 @@ func printInfo(msg string) {
 	printMessage(msg, colorPurple)
 }
 
-func printDebug(msg string) {
-	printMessage(msg, colorBlue)
-}
-
 func printWarning(msg string) {
 	printMessage(msg, colorYellow)
 }
 
 func printMessage(msg string, color string) {
+	isDebug := envParam("ZSH_PLUGINS_OH_MY_ALIASES_DEBUG", "0") == "1"
+	if isDebug {
+		msg = fmt.Sprintf("%s %s(took: %d μs)", msg, colorBlue, time.Since(start).Microseconds())
+	}
+
 	_, _ = fmt.Fprintln(os.Stdout, fmt.Sprintf("%s%s%s", color, msg, colorReset))
 }
 
